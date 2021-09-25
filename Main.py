60ef2e5f50229737064054bb6f75303e361588e5
Yeetzsche <91353201+Yeetzsche@users.noreply.github.com>
2021-09-25 11:35:29 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,13 +1,10 @@
 import PFR_Scrape_Aggregate as pfr
 
-year = 2021
-week = 2
+year = 2020
+week = 17
 multiple_weeks = True
 
 #Call the tools
+
 pfr.readData(year=year,week=week,multiple_weeks=multiple_weeks)
-
-for i in range(1,week+1):
-    
-    pfr.buildAggregate(year,i)
-
+pfr.buildAggregate(year=year,week=week,multiple_weeks=multiple_weeks)
